e98e9f7656153480be7a923439f9dd162e83084d
Ala <alarojek1@gmail.com>
2023-06-04 13:42:53 +0200

    wstawienie listy mealow do grila

---
+++ GrillFrontend/Converters/ImplementsInterfaceConverter.cs
@@ -0,0 +1,29 @@
+﻿using GrillBackend.Models.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GrillFrontend.Converters
+{
+    public class ImplementsInterfaceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IGrillable yourObject)
+            {
+                return yourObject is IGrillable;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
